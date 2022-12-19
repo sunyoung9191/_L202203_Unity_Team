1e7012ba4c8283f06fcedfc6d4eabf54fe180abb
euneei <87760320+euneei@users.noreply.github.com>
2022-12-19 09:06:09 +0900

    he_9

--- Assets/UI_SetActive_ground.cs
+++ Assets/UI_SetActive_ground.cs
@@ -9,9 +9,9 @@ public class UI_SetActive_ground : MonoBehaviour
 
     void Start()
     {
-        GameOverText.GetComponent<Text>().text = "도시 에셋, 테린 추가 : 박현서" +
+        GameOverText.GetComponent<Text>().text = "도시 에셋, 테린 추가 : 박현서\n" +
             "각각 메시 스크립트 구현: 박현서 - Mesh_hyunseo, 박선영 - Mesh_S" +
-            "ui 사용 메뉴얼 구현: 박현서" +
+            "ui 사용 메뉴얼 구현: 박현서\n" +
             "ground에서 heave, under로 이동: 박현서";
         if (GameOverText.enabled == true)
         {
