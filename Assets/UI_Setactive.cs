34bc5c175a5f53e53370c624f87bf3d48f9ea4c4
sunyoung9191 <102967598+sunyoung9191@users.noreply.github.com>
2022-12-19 14:41:55 +0900

    ani
    nksd

--- Assets/UI_Setactive.cs
+++ Assets/UI_Setactive.cs
@@ -9,7 +9,7 @@ public class UI_Setactive : MonoBehaviour
 
     void Start()
     {
-        GameOverText.GetComponent<Text>().text = "지형, 안개, 물, 바람 효과, 에셋 추가, 디자인: 박선영 박현서" +
+        GameOverText.GetComponent<Text>().text = "지형, 안개, 물, 바람 효과, 에셋 추가, 디자인, 애니메이터 컴포넌트 스피드 속성 제어: 박선영 박현서" +
             "비너스 이미지 세이더 삽입(세이더 다루기 7-10): 박현서" +
             "고래, 행성, 신전, 사자상 흑백 세이더 삽입, 회전: 박선영" +
             "이미지 이펙트 세이더(invert, gray,depth,색상혼합):박현서" +
