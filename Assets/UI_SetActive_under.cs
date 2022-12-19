96dd99693c92677ec941180d61bd4a6a495f087e
minj22 <mjmolang@gmail.com>
2022-12-19 15:54:04 +0900

    under_12

--- Assets/UI_SetActive_under.cs
+++ Assets/UI_SetActive_under.cs
@@ -9,7 +9,7 @@ public class UI_SetActive_under : MonoBehaviour
 
     void Start()
     {
-        GameOverText.GetComponent<Text>().text 
+        GameOverText.GetComponent<Text>().text
             = "전체적인 디자인 및 오브젝트 배치 : 양민지, 오하은, 윤하은\n"
             + "효과(사운드,Particle) 추가 : 윤하은, 양민지\n"
             + "조명 및 Terrain : 오하은\n"
@@ -19,21 +19,22 @@ public class UI_SetActive_under : MonoBehaviour
             + "Explosion : 양민지 윤하은\n"
             + "Eagle_Normal : 윤하은\n"
             + "Rubble_3 : 양민지\n"
+            + "Post-Process Layer 및 Post-process Volume 카메라 후처리: 윤하은\n"
+            + "Flashlight에 Spot 조명 효과: 윤하은\n"
             + "Camera_minimap, Minimap : 윤하은\n"
+            + "Mirror 효과 : 윤하은\n"
             + "Entrance : 양민지\n"
             + "Tree, Altar_2, Wolf_howl,Wolf : 양민지\n"
             + "Cube(지하에서 지상으로 이동) : 양민지\n"
-            + "stairs_1 (1) : 오하은\n"
-            + "Chair, Chair(1) : 오하은\n"
+            + "stairs_1 (1), Chair, Chair(1),Fences : 오하은\n"
             + "StrangeCube : 오하은\n"
             + "skull : 양민지\n"
             + "Rock 관련 오브젝트 셰이더 : 양민지\n"
-            +"Statues, Plates : 오하은\n"
-            + "Column : 양민지\n" +
+            + "Statues, Plates : 오하은\n"
+            + "Column, Zombies 추가: 양민지\n" +
             "Image Effect : 윤하은\n"
-            +"Cerabus 추가 및 애니매이터 : 오하은\n"
-            +"Zombies 추가 : 양민지\n"
-            +"Fences : 오하은\n";
+            + "Cerabus 추가 및 애니매이터 : 오하은\n";
+            
         if (GameOverText.enabled == true)
         {
             GameOverText.enabled = false;
